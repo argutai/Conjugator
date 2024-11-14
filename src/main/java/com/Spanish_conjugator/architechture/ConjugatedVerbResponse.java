fa4c04880a6fc20d1b4a548bf6058037be888414
Abigail <argutai@hotmail.com>
2024-11-14 00:32:57 +0000

    remove comments

--- src/main/java/com/Spanish_conjugator/architechture/ConjugatedVerbResponse.java
+++ src/main/java/com/Spanish_conjugator/architechture/ConjugatedVerbResponse.java
@@ -4,17 +4,14 @@ public class ConjugatedVerbResponse {
 
     private String conjugatedVerb;
 
-    // Constructor
     public ConjugatedVerbResponse(String conjugatedVerb) {
         this.conjugatedVerb = conjugatedVerb;
     }
 
-    // Getter
     public String getConjugatedVerb() {
         return conjugatedVerb;
     }
 
-    // Setter
     public void setConjugatedVerb(String conjugatedVerb) {
         this.conjugatedVerb = conjugatedVerb;
     }

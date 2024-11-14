3cbc2a06cba9579d1f84515a9f693c5e77c1b343
Abigail <argutai@hotmail.com>
2024-11-14 00:36:10 +0000

    Add service to choose which List<IrregularVerb> to chose by tense

---
+++ src/main/java/com/Spanish_conjugator/architechture/IrregularService.java
@@ -0,0 +1,41 @@
+package com.Spanish_conjugator.architechture;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.Spanish_conjugator.IrregularVerb;
+import com.Spanish_conjugator.Irregulars.ImperfectIrregular;
+import com.Spanish_conjugator.Irregulars.PresentIrregular;
+import com.Spanish_conjugator.Irregulars.PreteriteIrregular;
+
+@Service
+public class IrregularService {
+
+    public Optional<String> getIrregularConjugation(String verb, String form, String tense) {
+        // Choose the correct list based on the tense
+        List<IrregularVerb> irregularVerbs = selectIrregularVerbs(tense);
+
+
+        return irregularVerbs.stream()
+                .filter(irregularVerb -> irregularVerb.getVerb().equals(verb))
+                .map(irregularVerb -> irregularVerb.getConjugations().get(form))
+                .findFirst();
+    }
+
+    private List<IrregularVerb> selectIrregularVerbs(String tense) {
+        switch (tense.toLowerCase()) {
+            case "present" -> {
+                return PresentIrregular.IRREGULAR_VERBS;
+            }
+            case "preterite" -> {
+                return PreteriteIrregular.IRREGULAR_VERBS;
+            }
+            case "imperfect" -> {
+                return ImperfectIrregular.IRREGULAR_VERBS;
+            }
+            default -> throw new IllegalArgumentException("Unsupported tense: " + tense);
+        }
+    }
+}

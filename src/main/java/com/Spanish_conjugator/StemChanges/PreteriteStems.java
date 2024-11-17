4beec22ce4a9f9e9fcc15d6bf2dc7d6c4c057421
Abigail <argutai@hotmail.com>
2024-11-17 17:26:28 +0000

    add stem changers from irregulars

--- src/main/java/com/Spanish_conjugator/StemChanges/PreteriteStems.java
+++ src/main/java/com/Spanish_conjugator/StemChanges/PreteriteStems.java
@@ -7,8 +7,9 @@ import java.util.function.Function;
 public class PreteriteStems {
 
     public static final Map<Function<String, String>, List<String>> PRETERITE_STEM_CHANGE_RULES = Map.of(
-        stem -> stem.replace("e", "i"), List.of("pedir", "sentir", "divertir", "repetir", "servir", "vestir")
-        // stem -> stem.replace("o", "u"), List.of("dormir", "morir")
+        stem -> stem.replace("e", "i"), List.of("pedir", "sentir", "divertir", "repetir", "servir", "vestir", "preferir", "mentir"),
+        stem -> stem.replace("o", "u"), List.of("dormir", "morir")
+        // ToDo: ending in vowel, add y verbs
     );
 
 }

13f45ea20e9b2b9afa6ca89d2fb4c2e911d2d1ea
Abigail <argutai@hotmail.com>
2024-11-18 15:45:09 +0000

    switch result and actual

--- src/test/java/com/Spanish_conjugator/EndingServiceTest.java
+++ src/test/java/com/Spanish_conjugator/EndingServiceTest.java
@@ -14,65 +14,104 @@ class EndingServiceTest {
     @Test
     void testPresent1s() {
         String result = endingService.getEnding("present", "1s", "hablar");
-        assertEquals(result, "o");
+        assertEquals("o", result);
     }    
 
     @Test
     void testPresent2s() {
         String result = endingService.getEnding("present", "2s", "conocer");
-        assertEquals(result, "es");
+        assertEquals("es", result);
     }      
 
     // PRETERITE
     @Test
     void testPreterite3s() {
         String result = endingService.getEnding("preterite", "3s", "pintar");
-        assertEquals(result, "ó");
+        assertEquals("ó", result);
     }    
 
     @Test
     void testPreterite1p() {
         String result = endingService.getEnding("preterite", "1p", "comer");
-        assertEquals(result, "imos");
+        assertEquals("imos", result);
     }    
 
     // IMPERFECT
     @Test
     void testImperfect2p() {
         String result = endingService.getEnding("imperfect", "2p", "vivir");
-        assertEquals(result, "íais");
+        assertEquals("íais", result);
     }    
 
     @Test
     void testImperfect3p() {
         String result = endingService.getEnding("imperfect", "3p", "hablar");
-        assertEquals(result, "aban");
+        assertEquals("aban", result);
     }     
 
     // FUTURE
     @Test
     void testFuture1s() {
         String result = endingService.getEnding("future", "1s", "tener");
-        assertEquals(result, "é");
+        assertEquals("é", result);
     }    
 
     @Test
     void testFuture2s() {
         String result = endingService.getEnding("future", "2s", "vivir");
-        assertEquals(result, "ás");
+        assertEquals("ás", result);
     }    
 
     // CONDITIONAL
     @Test
     void testConditional3s() {
         String result = endingService.getEnding("conditional", "3s", "hablar");
-        assertEquals(result, "ía");
+        assertEquals("ía", result);
     }    
 
     @Test
     void testConditional1p() {
         String result = endingService.getEnding("conditional", "1p", "saber");
-        assertEquals(result, "íamos");
+        assertEquals("íamos", result);
+    }    
+
+    // SUBJUNCTIVE PRESENT
+    @Test
+    void testSubjunctivePresent2p() {
+        String result = endingService.getEnding("subjunctive present", "2p", "hablar");
+        assertEquals("éis", result);
+    }    
+
+    @Test
+    void testSubjunctivePresent3p() {
+        String result = endingService.getEnding("subjunctive present", "3p", "saber");
+        assertEquals("an", result);
+    }     
+
+    // SUBJUNCTIVE IMPERFECT
+    @Test
+    void testSubjunctiveImperfect1s() {
+        String result = endingService.getEnding("subjunctive imperfect", "2p", "hablar");
+        assertEquals("rais", result);
+    }    
+
+    @Test
+    void testSubjunctiveImperfect2s() {
+        String result = endingService.getEnding("subjunctive imperfect", "3p", "saber");
+        assertEquals("ran", result);
+    }    
+
+    // SUBJUNCTIVE FUTURE
+    @Test
+    void testSubjunctiveFuture3s() {
+        String result = endingService.getEnding("subjunctive future", "3s", "hablar");
+        assertEquals("re", result);
+    }    
+
+    @Test
+    void testSubjunctiveFuture1p() {
+        String result = endingService.getEnding("subjunctive future", "1p", "saber");
+        assertEquals("remos", result);
     }    
 
 }
